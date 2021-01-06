2d8e70429e5c4c619dd5d282db22d7fd7d491012
Muhammad Kashif <44mkashif@gmail.com>
2021-01-07 03:38:06 +0500

    Faculty endpoints updated

--- server/app/validation/faculty.js
+++ server/app/validation/faculty.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 module.exports = (faculty, update) => {
     let schema = Joi.object({
         name: Joi.string().min(3).max(50).required(),
-        location: Joi.string().min(5).max(255),
+        location: Joi.string().max(255),
         dean_id: Joi.number(),
         contact_phone: Joi.string().min(4).max(15).required(),
         contact_email: Joi.string().min(5).max(50).required().email(),
@@ -12,7 +12,7 @@ module.exports = (faculty, update) => {
     if (update) {
         schema = Joi.object({
             name: Joi.string().min(3).max(50),
-            location: Joi.string().min(5).max(255),
+            location: Joi.string().max(255),
             dean_id: Joi.number(),
             contact_phone: Joi.string().min(4).max(15),
             contact_email: Joi.string().min(5).max(50).email(),
